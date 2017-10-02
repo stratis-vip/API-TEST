9c370c97b631e1fc61ddd9c29850d9c77b4e7d35
Στρατής Χριστοδούλου <stratis.vip@gmail.com>
2017-10-02 19:53:02 +0300

    fix index at posts

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -1,11 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model(){
-        return this.store.findAll('post');
-    },
 
-    setupController(controller, model){
-        Ember.set(controller, 'posts', model)
-    }
 });
